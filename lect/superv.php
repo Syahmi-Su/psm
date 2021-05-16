796761c3e03f89d8fa03e487e72c5f36dd5a555d
Syahmi-Su <syahm.asd@gmail.com>
2021-05-17 00:33:25 +0800

    Update superv.php

--- lect/superv.php
+++ lect/superv.php
@@ -108,13 +108,6 @@
 
       <div class="container-fluid">
 
-        <!-- Breadcrumbs-->
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item">
-            <a href="lecture.php">Dashboard</a>
-          </li>
-          <li class="breadcrumb-item active">Tables</li>
-        </ol>
 
         <!-- DataTables Example -->
         <div class="card mb-3">
