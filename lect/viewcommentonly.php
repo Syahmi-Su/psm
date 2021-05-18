e458885d2eb86f69f663ee310de085749db3e86f
Syahmi-Su <syahm.asd@gmail.com>
2021-05-18 19:05:24 +0800

    update

--- lect/viewcommentonly.php
+++ lect/viewcommentonly.php
@@ -116,14 +116,6 @@ $row = mysqli_fetch_array($result);
 
       <div class="container-fluid">
 
-        <!-- Breadcrumbs-->
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item">
-            <a href="admin.php">Dashboard</a>
-          </li>
-          <li class="breadcrumb-item active">Tables</li>
-        </ol>
-
         <!-- DataTables Example -->
         <div class="card mb-3">
           <div class="card-header">
--- student/dbsession.php
+++ student/dbsession.php
@@ -5,7 +5,7 @@
         session_start();
     }
 
-    if (isset($_SESSION['c_ic']) != session_id())
+    if (isset($_SESSION['studMatric']) != session_id())
     {
          header('Location: Index.php');
     }
